7ab5d0c0faf9561aaa849a7cf1130ad7d9480e5f
Emily Vanaselja <you@example.com>
2023-10-24 19:05:32 +0000

    Added description

--- vanaselja.pkg/R/bar_graph.R
+++ vanaselja.pkg/R/bar_graph.R
@@ -1,6 +1,16 @@
-bar_graph <- function(data, x, y) {
+#' Creates a bar graph from any data that is put into the function.
+#' 
+#' Use the data from a data file to create a bar graph that represents the data and the counts of that data.
+#' 
+#' @param data The csv being graphed, needs to be in a data frame or data table 
+#' @param x The variable that will be on the x-axis of the graph, the bar graph counts the amount of this variable.
+#' @return plot A bar graph that shows the number of counts of the x-axis. 
+#' 
+#' @export 
+
+bar_graph <- function(data, x) {
   plot <- ggplot(data, mapping = aes({{x}})) + geom_bar()
   return(plot)
 }
 
-#Creates a bar graph from any data that is put into the function.
+
